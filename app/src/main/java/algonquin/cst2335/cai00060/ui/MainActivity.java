c5349da3b2ae3db1819db396b5948f6d55654d07
Yutian Cai <cai00060@algonquinlive.com>
2023-05-19 16:51:05 -0400

    Week2-Lab2-part4: finished  setOnClickListener() and MutableLiveData<String>

---
+++ app/src/main/java/algonquin/cst2335/cai00060/ui/MainActivity.java
@@ -0,0 +1,31 @@
+package algonquin.cst2335.cai00060.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import algonquin.cst2335.cai00060.data.MainViewModel;
+import algonquin.cst2335.cai00060.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private MainViewModel model;
+    private ActivityMainBinding variableBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // calls parent onCreate()
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot()); // load XML on screen
+
+        variableBinding.myButton.setOnClickListener(click ->
+                model.editString.postValue(variableBinding.myEditText.getText().toString()));
+        model.editString.observe(this, s ->
+                variableBinding.myText.setText("Your edit text has " + s));
+
+    }
+
+}
