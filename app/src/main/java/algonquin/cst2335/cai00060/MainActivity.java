818080f2562c8e6861566e70037959dff3520904
Yutian Cai <cai00060@algonquinlive.com>
2023-06-07 18:59:18 -0400

    Week4 finished

--- app/src/main/java/algonquin/cst2335/cai00060/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cai00060/MainActivity.java
@@ -2,7 +2,9 @@ package algonquin.cst2335.cai00060;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,17 +20,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
         Log.w(TAG, "In onCreate() - Loading Widgets");
 
-        // change here
-        // Button loginButton = findViewById(R.id.loginButton);
-        // EditText emailEditText = findViewById(R.id.emailEditText);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        float hi = prefs.getFloat("Hi", 0);
+        int age = prefs.getInt("Age", 0);
+        variableBinding.emailEditText.setText(emailAddress);
 
         variableBinding.loginButton.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", variableBinding.emailEditText.getText().toString());
+            editor.putFloat("Hi", 4.5f);
+            editor.putInt("Age", 35);
+            editor.apply();
             // Intent tells you where to go next
             // SecondActivity.class calls the constructor
             Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
--- app/src/main/java/algonquin/cst2335/cai00060/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/cai00060/SecondActivity.java
@@ -6,15 +6,27 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import algonquin.cst2335.cai00060.databinding.ActivityMainBinding;
 import algonquin.cst2335.cai00060.databinding.ActivitySecondBinding;
@@ -22,40 +34,74 @@ import algonquin.cst2335.cai00060.databinding.ActivitySecondBinding;
 public class SecondActivity extends AppCompatActivity {
 
     private ActivitySecondBinding variableBinding;
+    private ActivityResultLauncher<Intent> cameraResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_second);
         variableBinding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        Intent fromPrevious = getIntent();
-        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
-
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        // String emailAddress = fromPrevious.getStringExtra("EmailAddress");
         variableBinding.textView.setText("Welcome back " + emailAddress);
+        String phoneNumber = prefs.getString("phoneNumber", "");
+        variableBinding.editTextPhone.setText(phoneNumber);
+
+        Intent fromPrevious = getIntent();
+
+        File file = new File(getFilesDir(), "Picture.png");
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+            variableBinding.profileImage.setImageBitmap(theImage);
+        }
+
+        // move cameraResult from "inside setOnClickListener method" to "inside onCreate method"
+        cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            variableBinding.profileImage.setImageBitmap(thumbnail);
+                            FileOutputStream fOut = null;
+                            try {
+                                fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                                thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                fOut.flush();
+                                fOut.close();
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
 
         variableBinding.buttonChange.setOnClickListener(v -> {
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-//                                if (data != null && data.getExtras() != null) {
-                            Bitmap thumbnail = data.getParcelableExtra("data");
-                            variableBinding.profileImage.setImageBitmap(thumbnail);
-//                                }
-//                            }else if(result.getResultCode() == Activity.RESULT_CANCELED && data!=null) {
-//                                Toast.makeText(SecondActivity.this, "No Picture Taken", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    startActivity(cameraIntent);
+                else
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 20);
+            }
             cameraResult.launch(cameraIntent);
         });
 
         variableBinding.buttonCall.setOnClickListener(v -> {
             Intent call = new Intent(Intent.ACTION_DIAL);
-            call.setData(Uri.parse("tel:" + variableBinding.editTextPhone.getText().toString()));
+            String editPhoneNumber = variableBinding.editTextPhone.getText().toString();
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("phoneNumber", editPhoneNumber);
+            editor.apply();
+
+            call.setData(Uri.parse("tel:" + editPhoneNumber));
             startActivity(call);
         });
 
@@ -63,6 +109,14 @@ public class SecondActivity extends AppCompatActivity {
             // go back to the last page where you are from
             finish();
         });
+    }
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("phoneNumber", variableBinding.editTextPhone.getText().toString());
     }
 }
