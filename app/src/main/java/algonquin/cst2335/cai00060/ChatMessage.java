0391cbdc89d5f48890547d025261b681bd4704b7
Yutian Cai <cai00060@algonquinlive.com>
2023-07-07 22:41:31 -0400

    Week7 finished first step

--- app/src/main/java/algonquin/cst2335/cai00060/ChatMessage.java
+++ app/src/main/java/algonquin/cst2335/cai00060/ChatMessage.java
@@ -1,25 +1,55 @@
 package algonquin.cst2335.cai00060;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class ChatMessage {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    public long id;
+
+    @ColumnInfo(name = "message")
     private String message;
+
+    @ColumnInfo(name = "timeSent")
     private String timeSent;
+
+    @ColumnInfo(name = "isSentButton")
     private boolean isSentButton;
 
-    ChatMessage(String m, String t, boolean sent) {
-        message = m;
-        timeSent = t;
-        isSentButton = sent;
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String m, String t, boolean sent) {
+        setMessage(m);
+        setTimeSent(t);
+        setIsSentButton(sent);
     }
 
     public String getMessage() {
         return message;
     }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public String getTimeSent() {
         return timeSent;
     }
 
+    public void setTimeSent(String timeSent) {
+        this.timeSent = timeSent;
+    }
+
     public boolean getIsSentButton() {
         return isSentButton;
+    }
+
+    public void setIsSentButton(boolean isSentButton) {
+        this.isSentButton = isSentButton;
     }
 }
--- app/src/main/java/algonquin/cst2335/cai00060/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/cai00060/ChatRoom.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,9 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import algonquin.cst2335.cai00060.databinding.ActivityChatRoomBinding;
 import algonquin.cst2335.cai00060.databinding.ReceiveMessageBinding;
@@ -21,10 +25,11 @@ import algonquin.cst2335.cai00060.databinding.SentMessageBinding;
 
 public class ChatRoom extends AppCompatActivity {
 
-    private ActivityChatRoomBinding binding;
-    private ArrayList<ChatMessage> messages;
-    private ChatRoomViewModel chatModel;
-    private RecyclerView.Adapter myAdapter;
+    protected ActivityChatRoomBinding binding;
+    protected ArrayList<ChatMessage> messages;
+    protected ChatRoomViewModel chatModel;
+    protected RecyclerView.Adapter myAdapter;
+    protected ChatMessageDAO myDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +38,9 @@ public class ChatRoom extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
-        // initialize to the ViewModel arraylist
-        messages = chatModel.messages.getValue();
-        if (messages == null) {
-            chatModel.messages.postValue(messages = new ArrayList<ChatMessage>());
-        }
 
-        binding.sendButton.setOnClickListener(click -> {
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
-            String currentDateAndTime = sdf.format(new Date());
-            messages.add(new ChatMessage(binding.textInput.getText().toString(), currentDateAndTime, true));
-            myAdapter.notifyItemInserted(messages.size() - 1);
-            //clear the previous text
-            binding.textInput.setText("");
-        });
-
-        binding.receiveButton.setOnClickListener(click -> {
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
-            String currentDateAndTime = sdf.format(new Date());
-            messages.add(new ChatMessage(binding.textInput.getText().toString(), currentDateAndTime, false));
-            myAdapter.notifyItemInserted(messages.size() - 1);
-            //clear the previous text
-            binding.textInput.setText("");
-        });
-
-        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+        // initialize myAdapter
+        myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -87,6 +70,75 @@ public class ChatRoom extends AppCompatActivity {
                 if (obj.getIsSentButton() == true) return 0;
                 else return 1;
             }
+        };
+
+        //access the database
+        MessageDatabase db = Room.databaseBuilder(getApplicationContext(), MessageDatabase.class, "MyChatMessageDatabase").build();
+        myDAO = db.cmDAO();
+
+//        if(messages == null) {
+//            chatModel.messages.setValue(messages = new ArrayList<>());
+//            Executor thread = Executors.newSingleThreadExecutor();
+//            thread.execute(() -> {
+//                messages.addAll( myDAO.getAllMessages() ); //Once you get the data from database
+//                runOnUiThread( () ->  binding.recycleView.setAdapter( myAdapter )); //You can then load the RecyclerView
+//            });
+//        }
+
+        //get all messages from database
+        Executor thread = Executors.newSingleThreadExecutor();
+        thread.execute(() -> {
+            // run on a second processor
+            List<ChatMessage> fromDatabase = myDAO.getAllMessages();
+            messages.addAll(fromDatabase);
+            //set adapter
+            binding.recycleView.setAdapter(myAdapter);
+            // update the recycler view
+            runOnUiThread(() -> binding.recycleView.setAdapter(myAdapter));
+        });
+
+        // initialize to the ViewModel arraylist
+        messages = chatModel.messages.getValue();
+        if (messages == null) {
+            chatModel.messages.postValue(messages = new ArrayList<ChatMessage>());
+        }
+
+        binding.sendButton.setOnClickListener(click -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+            String currentDateAndTime = sdf.format(new Date());
+            ChatMessage newChatMessage = new ChatMessage(binding.textInput.getText().toString(), currentDateAndTime, true);
+            //insert into ArrayList
+            messages.add(newChatMessage);
+            //insert into database. It returns the id of this new insertion
+            Executor threadA = Executors.newSingleThreadExecutor();
+            threadA.execute(() -> {
+                // run on a second processor
+                newChatMessage.id = myDAO.insertMessage(newChatMessage);
+            });
+            //update the rows
+            myAdapter.notifyItemInserted(messages.size() - 1);
+            // myAdapter.notifyDataSetChanged();
+
+            //clear the previous text
+            binding.textInput.setText("");
+        });
+
+        binding.receiveButton.setOnClickListener(click -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+            String currentDateAndTime = sdf.format(new Date());
+            ChatMessage newChatMessage = new ChatMessage(binding.textInput.getText().toString(), currentDateAndTime, false);
+            //insert into ArrayList
+            messages.add(newChatMessage);
+            //insert into database. It returns the id of this new insertion
+            Executor threadA = Executors.newSingleThreadExecutor();
+            threadA.execute(() -> {
+                // run on a second processor
+                newChatMessage.id = myDAO.insertMessage(newChatMessage);
+            });
+            //update the rows
+            myAdapter.notifyItemInserted(messages.size() - 1);
+            //clear the previous text
+            binding.textInput.setText("");
         });
 
         // To specify a single column scrolling in a Vertical direction
